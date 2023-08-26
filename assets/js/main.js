d52977e34efc044bd1c1239e0d37964f03fab4f2
HeberRios <68972507+HeberRios@users.noreply.github.com>
2023-08-26 12:15:17 -0600

    Display the pressed number or operator
    Created functions to display the pressed number or operator in the
    calculator screen. For this was also added dataset attributes to the
    buttons in the html. Lastly when the screen its updated the value its
    stored.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 "use strict";
 
+// ELEMENTS --------------------------------------------------------------
+const numbers = document.querySelectorAll("[data-number]");
+
+//operators
+const operators = document.querySelectorAll("[data-operator]");
+const displayOutput = document.getElementById("display-output");
+displayOutput.textContent = "";
+
 // VARIABLES -------------------------------------------------------------
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
+let displayContent = "";
 
 // FUNCTIONS -------------------------------------------------------------
 
@@ -36,3 +45,61 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+function displayPressedNumber(button) {
+    if (button.dataset.number === "1") {
+        displayOutput.textContent += "1";
+    } else if (button.dataset.number === "2") {
+        displayOutput.textContent += "2";
+    } else if (button.dataset.number === "3") {
+        displayOutput.textContent += "3";
+    } else if (button.dataset.number === "4") {
+        displayOutput.textContent += "4";
+    } else if (button.dataset.number === "5") {
+        displayOutput.textContent += "5";
+    } else if (button.dataset.number === "6") {
+        displayOutput.textContent += "6";
+    } else if (button.dataset.number === "7") {
+        displayOutput.textContent += "7";
+    } else if (button.dataset.number === "8") {
+        displayOutput.textContent += "8";
+    } else if (button.dataset.number === "9") {
+        displayOutput.textContent += "9";
+    } else if (button.dataset.number === "0") {
+        displayOutput.textContent += "0";
+    }
+}
+
+function displayPressedOperator(operator) {
+    if (operator.dataset.operator === "+") {
+        displayOutput.textContent += "+";
+    } else if (operator.dataset.operator === "-") {
+        displayOutput.textContent += "-";
+    } else if (operator.dataset.operator === "*") {
+        displayOutput.textContent += "x";
+    } else if (operator.dataset.operator === "/") {
+        displayOutput.textContent += "/";
+    }
+}
+
+function saveDisplayContent() {
+    displayContent = displayOutput.textContent;
+}
+
+// POPULATE THE DISPLAY
+
+//NUMBERS
+numbers.forEach((number) => {
+    number.addEventListener("click", function () {
+        displayPressedNumber(number);
+        saveDisplayContent();
+    });
+});
+
+// OPERATORS
+operators.forEach((operator) => {
+    operator.addEventListener("click", function () {
+        displayPressedOperator(operator);
+        saveDisplayContent();
+    });
+});
