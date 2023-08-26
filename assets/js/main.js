67498f4767812180a5caea0fb6d51b054714e28b
HeberRios <68972507+HeberRios@users.noreply.github.com>
2023-08-26 10:27:12 -0600

    Add basic math functions
    Added the basic math functions of add, subtract, multiply and divide.
    Also created variables for the arguments of the functions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,38 @@
 "use strict";
+
+// VARIABLES -------------------------------------------------------------
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+// FUNCTIONS -------------------------------------------------------------
+
+// BASIC MATH FUNCTIONS
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+}
