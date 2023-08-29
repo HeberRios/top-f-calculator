4255627b0b522356fa1ce09da0f7ce88e92dabc5
HeberRios <68972507+HeberRios@users.noreply.github.com>
2023-08-29 11:16:16 -0600

    Create functions to populate the display
    The necessary functions to populate the display when a number or an
    operator is pressed were added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const operators = document.querySelectorAll("[data-operator]");
 const actions = document.querySelectorAll("[data-action]");
 
 // VARIABLES -------------------------------------------------------------
+let displayValue = "";
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
@@ -42,3 +43,61 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+function displayPressedNumber(number) {
+    if (number.dataset.number === "1") {
+        displayOutput.textContent += "1";
+    } else if (number.dataset.number === "2") {
+        displayOutput.textContent += "2";
+    } else if (number.dataset.number === "3") {
+        displayOutput.textContent += "3";
+    } else if (number.dataset.number === "4") {
+        displayOutput.textContent += "4";
+    } else if (number.dataset.number === "5") {
+        displayOutput.textContent += "5";
+    } else if (number.dataset.number === "6") {
+        displayOutput.textContent += "6";
+    } else if (number.dataset.number === "7") {
+        displayOutput.textContent += "7";
+    } else if (number.dataset.number === "8") {
+        displayOutput.textContent += "8";
+    } else if (number.dataset.number === "9") {
+        displayOutput.textContent += "9";
+    } else if (number.dataset.number === "0") {
+        displayOutput.textContent += "0";
+    }
+}
+
+function displayPressedOperator(operator) {
+    if (operator.dataset.operator === "+") {
+        displayOutput.textContent += "+";
+    } else if (operator.dataset.operator === "-") {
+        displayOutput.textContent += "-";
+    } else if (operator.dataset.operator === "x") {
+        displayOutput.textContent += "x";
+    } else if (operator.dataset.operator === "/") {
+        displayOutput.textContent += "/";
+    }
+}
+
+function saveDisplayValue() {
+    displayValue = displayOutput.textContent;
+}
+
+// EVENT LISTENERS -------------------------------------------------------
+// Numbers
+numbers.forEach((number) => {
+    number.addEventListener("click", function () {
+        displayPressedNumber(number);
+        saveDisplayValue();
+    });
+});
+
+// Operators
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", function () {
+        displayPressedOperator(operator);
+        saveDisplayValue();
+    });
+});
